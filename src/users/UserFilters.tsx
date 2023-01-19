6bfa733e7c19d35779d3253f47580d66278dfdcb
Ilias Deros <IliasDeros@users.noreply.github.com>
2023-01-19 15:51:42 +0000

    Make CI's tslint ☹️ -> 😊

--- src/users/UserFilters.tsx
+++ src/users/UserFilters.tsx
@@ -1,9 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import useFilterReducer, { Action } from './useFilterReducer'
-
-// Design had 431 / 1230 as filter
-const minAgeValue = 0
-const maxAgeValue = 9999
 
 type UserFiltersProps = {
   dispatchFilter: React.Dispatch<Action>
@@ -24,7 +20,7 @@ function UserFilters({
   useEffect(() => {
     dispatchValues({ type: 'SET_MIN_AGE', value: filter.minAge })
     dispatchValues({ type: 'SET_MAX_AGE', value: filter.maxAge })
-  }, [filter.minAge, filter.maxAge])
+  }, [dispatchValues, filter.minAge, filter.maxAge])
 
   const updateMin = (value: number) => dispatchValues({ type: 'SET_MIN_AGE', value })
   const updateMax = (value: number) => dispatchValues({ type: 'SET_MAX_AGE', value })
